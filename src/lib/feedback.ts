c1a089b5539228c0c4b2254fd1337eda65823a25
MaximAndriienko <61285453+MaximAndriienko@users.noreply.github.com>
2026-06-08 18:14:28 +0300

    chore: force english feedback form locale (#80)

--- src/lib/feedback.ts
+++ src/lib/feedback.ts
@@ -1,5 +1,5 @@
 export const FEEDBACK_URL =
-  "https://docs.google.com/forms/d/e/1FAIpQLSfRFRkfmqI-VnoXnTwcSY3E87BVyqvV2wMv1HbI3LlwMM-rMQ/viewform?usp=dialog";
+  "https://docs.google.com/forms/d/e/1FAIpQLSfRFRkfmqI-VnoXnTwcSY3E87BVyqvV2wMv1HbI3LlwMM-rMQ/viewform?hl=en";
 
 const PLACEHOLDER_VALUES = new Set([
   "",
